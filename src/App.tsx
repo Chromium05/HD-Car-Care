ae9ad16fec45417ca910678bc6cfbf08fe2d8a00
Muhammad Abiddar Putra Mirza <abiddarputra83@gmail.com>
2025-08-22 14:06:20 +0700

    change footer

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { About } from './components/About';
 import { Gallery } from './components/Gallery';
 import { Testimonials } from './components/Testimonials';
 import { Contact } from './components/Contact';
-import { Footer } from './components/Footer';
+import Footer from './components/Footer';
 import { WhatsAppButton } from './components/WhatsAppButton';
 
 export function App() {
